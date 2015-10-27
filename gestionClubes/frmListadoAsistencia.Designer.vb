8457345988b5c74e9e42443b710ac5df510487b5
cordonmm <macordonperez@gmail.com>
2015-10-27 14:18:13 +0100

    MERGE: Se han unido todos los cambios

--- gestionClubes/frmListadoAsistencia.Designer.vb
+++ gestionClubes/frmListadoAsistencia.Designer.vb
@@ -29,22 +29,22 @@ Partial Class frmListadoAsistencia
         Me.txtSocio = New System.Windows.Forms.TextBox()
         Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
         Me.MetroGrid1 = New MetroFramework.Controls.MetroGrid()
-        Me.VarelaDataSet3 = New GestionClubes.VarelaDataSet3()
+        Me.VarelaDataSet1 = New GestionClubes.VarelaDataSet1()
         Me.TableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.TableTableAdapter = New GestionClubes.VarelaDataSet3TableAdapters.TableTableAdapter()
-        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.DniDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.NombreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ApellidosDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EstadoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ObservacionesDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FechaaltaDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FechabajaDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmailDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TelefonoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.AsistenciaDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TableTableAdapter = New GestionClubes.VarelaDataSet1TableAdapters.TableTableAdapter()
+        Me.IdDataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.MetroGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.VarelaDataSet3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VarelaDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -81,7 +81,7 @@ Partial Class frmListadoAsistencia
         DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
         Me.MetroGrid1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
         Me.MetroGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.MetroGrid1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.DniDataGridViewTextBoxColumn, Me.NombreDataGridViewTextBoxColumn, Me.ApellidosDataGridViewTextBoxColumn, Me.EstadoDataGridViewTextBoxColumn, Me.ObservacionesDataGridViewTextBoxColumn, Me.FechaaltaDataGridViewTextBoxColumn, Me.FechabajaDataGridViewTextBoxColumn, Me.EmailDataGridViewTextBoxColumn, Me.TelefonoDataGridViewTextBoxColumn, Me.AsistenciaDataGridViewTextBoxColumn})
+        Me.MetroGrid1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7, Me.DataGridViewTextBoxColumn8, Me.DataGridViewTextBoxColumn9, Me.DataGridViewTextBoxColumn10})
         Me.MetroGrid1.DataSource = Me.TableBindingSource
         DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
@@ -110,85 +110,85 @@ Partial Class frmListadoAsistencia
         Me.MetroGrid1.Size = New System.Drawing.Size(1117, 260)
         Me.MetroGrid1.TabIndex = 100
         '
-        'VarelaDataSet3
+        'VarelaDataSet1
         '
-        Me.VarelaDataSet3.DataSetName = "VarelaDataSet3"
-        Me.VarelaDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        Me.VarelaDataSet1.DataSetName = "VarelaDataSet1"
+        Me.VarelaDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'TableBindingSource
         '
         Me.TableBindingSource.DataMember = "Table"
-        Me.TableBindingSource.DataSource = Me.VarelaDataSet3
+        Me.TableBindingSource.DataSource = Me.VarelaDataSet1
         '
         'TableTableAdapter
         '
         Me.TableTableAdapter.ClearBeforeFill = True
         '
-        'IdDataGridViewTextBoxColumn
+        'IdDataGridViewTextBoxColumn1
         '
-        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
-        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
-        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        Me.IdDataGridViewTextBoxColumn1.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn1.HeaderText = "Nº Socio"
+        Me.IdDataGridViewTextBoxColumn1.Name = "IdDataGridViewTextBoxColumn1"
         '
-        'DniDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn1
         '
-        Me.DniDataGridViewTextBoxColumn.DataPropertyName = "dni"
-        Me.DniDataGridViewTextBoxColumn.HeaderText = "dni"
-        Me.DniDataGridViewTextBoxColumn.Name = "DniDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "dni"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "dni"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
         '
-        'NombreDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn2
         '
-        Me.NombreDataGridViewTextBoxColumn.DataPropertyName = "nombre"
-        Me.NombreDataGridViewTextBoxColumn.HeaderText = "nombre"
-        Me.NombreDataGridViewTextBoxColumn.Name = "NombreDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "nombre"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "nombre"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
         '
-        'ApellidosDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn3
         '
-        Me.ApellidosDataGridViewTextBoxColumn.DataPropertyName = "apellidos"
-        Me.ApellidosDataGridViewTextBoxColumn.HeaderText = "apellidos"
-        Me.ApellidosDataGridViewTextBoxColumn.Name = "ApellidosDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "apellidos"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "apellidos"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
         '
-        'EstadoDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn4
         '
-        Me.EstadoDataGridViewTextBoxColumn.DataPropertyName = "estado"
-        Me.EstadoDataGridViewTextBoxColumn.HeaderText = "estado"
-        Me.EstadoDataGridViewTextBoxColumn.Name = "EstadoDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "estado"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "estado"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
         '
-        'ObservacionesDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn5
         '
-        Me.ObservacionesDataGridViewTextBoxColumn.DataPropertyName = "observaciones"
-        Me.ObservacionesDataGridViewTextBoxColumn.HeaderText = "observaciones"
-        Me.ObservacionesDataGridViewTextBoxColumn.Name = "ObservacionesDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "observaciones"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "observaciones"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
         '
-        'FechaaltaDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn6
         '
-        Me.FechaaltaDataGridViewTextBoxColumn.DataPropertyName = "fechaalta"
-        Me.FechaaltaDataGridViewTextBoxColumn.HeaderText = "fechaalta"
-        Me.FechaaltaDataGridViewTextBoxColumn.Name = "FechaaltaDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "fechaalta"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "fechaalta"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
         '
-        'FechabajaDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn7
         '
-        Me.FechabajaDataGridViewTextBoxColumn.DataPropertyName = "fechabaja"
-        Me.FechabajaDataGridViewTextBoxColumn.HeaderText = "fechabaja"
-        Me.FechabajaDataGridViewTextBoxColumn.Name = "FechabajaDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn7.DataPropertyName = "fechabaja"
+        Me.DataGridViewTextBoxColumn7.HeaderText = "fechabaja"
+        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
         '
-        'EmailDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn8
         '
-        Me.EmailDataGridViewTextBoxColumn.DataPropertyName = "email"
-        Me.EmailDataGridViewTextBoxColumn.HeaderText = "email"
-        Me.EmailDataGridViewTextBoxColumn.Name = "EmailDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn8.DataPropertyName = "email"
+        Me.DataGridViewTextBoxColumn8.HeaderText = "email"
+        Me.DataGridViewTextBoxColumn8.Name = "DataGridViewTextBoxColumn8"
         '
-        'TelefonoDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn9
         '
-        Me.TelefonoDataGridViewTextBoxColumn.DataPropertyName = "telefono"
-        Me.TelefonoDataGridViewTextBoxColumn.HeaderText = "telefono"
-        Me.TelefonoDataGridViewTextBoxColumn.Name = "TelefonoDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn9.DataPropertyName = "telefono"
+        Me.DataGridViewTextBoxColumn9.HeaderText = "telefono"
+        Me.DataGridViewTextBoxColumn9.Name = "DataGridViewTextBoxColumn9"
         '
-        'AsistenciaDataGridViewTextBoxColumn
+        'DataGridViewTextBoxColumn10
         '
-        Me.AsistenciaDataGridViewTextBoxColumn.DataPropertyName = "asistencia"
-        Me.AsistenciaDataGridViewTextBoxColumn.HeaderText = "asistencia"
-        Me.AsistenciaDataGridViewTextBoxColumn.Name = "AsistenciaDataGridViewTextBoxColumn"
+        Me.DataGridViewTextBoxColumn10.DataPropertyName = "asistencia"
+        Me.DataGridViewTextBoxColumn10.HeaderText = "asistencia"
+        Me.DataGridViewTextBoxColumn10.Name = "DataGridViewTextBoxColumn10"
         '
         'frmListadoAsistencia
         '
@@ -201,7 +201,7 @@ Partial Class frmListadoAsistencia
         Me.Name = "frmListadoAsistencia"
         Me.Text = "Listado de asistencia"
         CType(Me.MetroGrid1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.VarelaDataSet3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VarelaDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -210,10 +210,6 @@ Partial Class frmListadoAsistencia
     Friend WithEvents txtSocio As TextBox
     Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
     Friend WithEvents MetroGrid1 As MetroFramework.Controls.MetroGrid
-    Friend WithEvents VarelaDataSet3 As VarelaDataSet3
-    Friend WithEvents TableBindingSource As BindingSource
-    Friend WithEvents TableTableAdapter As VarelaDataSet3TableAdapters.TableTableAdapter
-    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents DniDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents NombreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents ApellidosDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
@@ -224,4 +220,18 @@ Partial Class frmListadoAsistencia
     Friend WithEvents EmailDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents TelefonoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents AsistenciaDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents VarelaDataSet1 As VarelaDataSet1
+    Friend WithEvents TableBindingSource As BindingSource
+    Friend WithEvents TableTableAdapter As VarelaDataSet1TableAdapters.TableTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn7 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn8 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn9 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn10 As DataGridViewTextBoxColumn
 End Class
